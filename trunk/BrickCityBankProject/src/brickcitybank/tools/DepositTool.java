c97fedeb96401b4482ce9f34b65033f980199c27
jeffreydmueller <jeffreydmueller@77c099dc-cc87-11dd-a8b6-f5aca7b582bb>
2009-02-18 22:47:16 +0000

    Transaction history works

--- trunk/BrickCityBankProject/src/brickcitybank/tools/DepositTool.java
+++ trunk/BrickCityBankProject/src/brickcitybank/tools/DepositTool.java
@@ -6,6 +6,8 @@ package brickcitybank.tools;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import brickcitybank.message.MessageOrder;
 import brickcitybank.message.MessageOrderMoney;
@@ -22,6 +24,9 @@ import exception.SumIncorrectException;
  */
 public class DepositTool extends ActionTool{
 
+	String old1, old2, new1, new2;
+	int successQuery;
+	
 	public DepositTool(DBConnection connector, ActionTool succ) {
 		super(connector, succ);
 		// TODO Auto-generated constructor stub
@@ -52,7 +57,8 @@ public class DepositTool extends ActionTool{
 	/**
 	 * 
 	 */
-	public MessageResponse executeAction(MessageOrder m) {
+	public MessageResponse executeAction(MessageOrder m) 
+	{
 		System.out.println("Top of executeAction in DepositTool");
 		if(((MessageOrderMoney)m).getSum()<0){
 			//can't accept the sum must be >0
@@ -71,20 +77,23 @@ public class DepositTool extends ActionTool{
 			//case of a loan, the jointure is not empty
 			rs = state.executeQuery("SELECT Balance FROM Account,Loan WHERE idAccount ="+m.getIdAcount()+" AND idLoan="+m.getIdAcount());
 			
-			if (rs.first()){
+			if (rs.first())
+			{
 				//means it's a loan
+				sumToUpdate = rs.getDouble("Balance");
 				
-					sumToUpdate = rs.getDouble("Balance");
-				
-				if (sumToUpdate >= ((MessageOrderMoney)m).getSum()){
+				if (sumToUpdate >= ((MessageOrderMoney)m).getSum())
+				{
 					
 					//means the sum to add is < than the current balance the user owes
 					//add it the the account (update)
 					sumToUpdate -= ((MessageOrderMoney)m).getSum();
-				}else{
+				}
+				else
+				{
 					//the user entered to much money for the loan
 					throw new  SumIncorrectException("Your Loan has not been updated, you try to add to much money, the current balance is "+sumToUpdate);
-					}
+				}
 			}
 			else
 			{
@@ -102,30 +111,84 @@ public class DepositTool extends ActionTool{
 				}
 			}
 			
-			int successQuery = state.executeUpdate("UPDATE Account set Balance="+sumToUpdate + " WHERE idAccount ="+m.getIdAcount()+" ");
-			System.out.println(successQuery +"<- successquery");
-			if (successQuery > 0)
-			{	
-				//success in updating
-				return new MessageResponse("Your Balance has been succesfully updated, the current balance is "+sumToUpdate);
-			}
-			else
+			try
 			{
-				//can't update
-				throw new  CannotUpdateException("Your Loan has not been updated, please try again");
+				// Get old balance
+				rs = state.executeQuery("select balance from account where idAccount =" +m.getIdAcount());
+				old1 = "0";
+				old2 = "0";
+				new1 = "0";
+				new2 = "0";
+				while(rs.next())
+				{
+					old1 = rs.getString(1);
+					new1 = old1;
+				}
 			}
-				
-		
-		}
-		catch (AccountNotFoundException e){
-				//when the Account is not found
+			catch(Exception e) {e.printStackTrace();}
+			
+			try
+			{
+				successQuery = state.executeUpdate("UPDATE Account set Balance="+sumToUpdate + " WHERE idAccount ="+m.getIdAcount()+" ");
+			}
+			catch(Exception e) {e.printStackTrace();}
+
+			try
+			{
+				rs = state.executeQuery("select balance from account where idAccount =" +m.getIdAcount());
+				while(rs.next())
+				{
+					old2 = rs.getString(1);
+					new2 = old2;
+				}
+	
+				if (successQuery > 0)
+				{	
+					//success in updating
+					
+					// Insert this transaction into transaction table
+					String DATE_FORMAT_NOW = "yyyy-MM-dd";
+					String TIME_FORMAT_NOW = "HH:mm:ss";
+					Calendar cal = Calendar.getInstance();
+					SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_NOW);
+					String date = sdf.format(cal.getTime());
+					sdf = new SimpleDateFormat(TIME_FORMAT_NOW);
+					String time = sdf.format(cal.getTime());
+					System.out.println("Date: " +date +" time: " +time);
+	
+					String query = "insert into transaction (type, account1, account2, Date, Time, old_balance1, new_balance1, old_balance2, new_balance2) " +
+					"values ('D', '"+m.getIdAcount() +"', '" +m.getIdAcount() +"', '" +date +"', '" +time +"', '" +old1 +"', '" +new1 +"', '" +old2 +"', '" +new2 +"')";
+					
+					System.out.println(query);
+					state.executeUpdate(query);
+					
+					return new MessageResponse("Your Balance has been succesfully updated, the current balance is "+sumToUpdate);
+				}
+				else
+				{
+					//can't update
+					throw new  CannotUpdateException("Your Loan has not been updated, please try again");
+				}
+			}
+			catch (CannotUpdateException e)
+			{
+				//when it's not possible to update the balance
 				return new MessageResponse(e.getMessage());
 			}
-		catch (CannotUpdateException e){
-			//when it's not possible to update the balance
+			catch(Exception e) 
+			{ 
+				e.printStackTrace();
+			}
+			
+		}
+		catch (AccountNotFoundException e)
+		{
+			//when the Account is not found
 			return new MessageResponse(e.getMessage());
 		}
-		catch (SumIncorrectException e){
+		
+		catch (SumIncorrectException e)
+		{
 			//when a user entered a non valid sum of money
 			return new MessageResponse(e.getMessage());
 		}
@@ -135,9 +198,9 @@ public class DepositTool extends ActionTool{
 			System.err.println("  Error Message: " + e.getMessage());
 			//System.err.println(" Vendor Message: " + e.getErrorCode());
 		}
+		catch(Exception e) { e.printStackTrace(); }
 		
-		// TODO Auto-generated method stub
 		return null;
 	}
-	}
+}
 }
--- trunk/BrickCityBankProject/src/brickcitybank/tools/WithdrawalTool.java
+++ trunk/BrickCityBankProject/src/brickcitybank/tools/WithdrawalTool.java
@@ -6,6 +6,8 @@ package brickcitybank.tools;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import brickcitybank.message.*;
 import database.DBConnection;
@@ -20,12 +22,15 @@ import exception.SumIncorrectException;
  */
 public class WithdrawalTool extends ActionTool{
 
+	String old1, old2, new1, new2;
+	int successQuery;
 	/**
 	 * Constructor
 	 * @param connector
 	 * @param succ
 	 */
-	public WithdrawalTool(DBConnection connector, ActionTool succ) {
+	public WithdrawalTool(DBConnection connector, ActionTool succ) 
+	{
 		super(connector, succ);
 		// TODO Auto-generated constructor stub
 	}
@@ -34,13 +39,16 @@ public class WithdrawalTool extends ActionTool{
 	/**
 	 * 
 	 */
-	public MessageResponse executeAction(MessageOrder m) {
-		
-		if(((MessageOrderMoney)m).getSum()<0){
+	public MessageResponse executeAction(MessageOrder m)
+	{
+		if(((MessageOrderMoney)m).getSum()<0)
+		{
 			//can't accept the sum must be >0
 			return new MessageResponse("Sum to withdraw must be positive!");
 			
-		}else{
+		}
+		else
+		{
 			Statement state = null;
 			ResultSet rs = null;
 			double sumToUpdate = 0;
@@ -50,65 +58,136 @@ public class WithdrawalTool extends ActionTool{
 				//first get the current balance of the Account
 				//case of a loan, the jointure is not empty
 				rs = state.executeQuery("SELECT Balance FROM Account WHERE idAccount ="+m.getIdAcount());
-					if (rs.first()){
-						//means it has an entry
-						
-						sumToUpdate = rs.getDouble("Balance");
-						
-						if(sumToUpdate <= ((MessageOrderMoney)m).getSum()){
-							//means that the user wants to withdraw more than he owes
-							throw new SumIncorrectException("You want to withdraw more than you have, your current balance is :"+sumToUpdate);
-			
-						}else{
-							//correct amount
-						
-							sumToUpdate -= ((MessageOrderMoney)m).getSum();
+				if (rs.first())
+				{
+					//means it has an entry
 					
+					sumToUpdate = rs.getDouble("Balance");
 					
-							int successQuery = state.executeUpdate("UPDATE Account set Balance="+sumToUpdate + " WHERE idAccount ="+m.getIdAcount()+" ");
-							
-							if (successQuery > 0)
-							{
-								//success in updating
-								return new MessageResponse("Your Balance has been succesfully updated, the current balance is "+sumToUpdate);
-							}
-							else
-							{
-								System.out.println(successQuery +"<- successquery");
-								//can't update
-								throw new  CannotUpdateException("Your Balance has not been updated, please try again");
-							}
-						}
-					}else
+					if(sumToUpdate <= ((MessageOrderMoney)m).getSum())
 					{
-						//the account number doesn't exist
-						throw new AccountNotFoundException();
+						//means that the user wants to withdraw more than he owes
+						throw new SumIncorrectException("You want to withdraw more than you have, your current balance is :"+sumToUpdate);
+		
 					}
-			}
-			catch (AccountNotFoundException e)
-			{
-				//when the Account is not found
-				return new MessageResponse(e.getMessage());
-			}
-			catch (CannotUpdateException e)
-			{
-				//when it's not possible to update the balance
-				return new MessageResponse(e.getMessage());
-			}
-			catch (SumIncorrectException e)
-			{
-				//when a user entered a non valid sum of money
-				return new MessageResponse(e.getMessage());
-			}
-			catch(SQLException e)
-			{
-				System.err.println(e.getMessage());
-				System.err.println("  Error Message: " + e.getMessage());
-				//System.err.println(" Vendor Message: " + e.getErrorCode());
-			}
-			
+					else
+					{
+						//correct amount
+					
+						sumToUpdate -= ((MessageOrderMoney)m).getSum();
+				
+				
+						int successQuery = state.executeUpdate("UPDATE Account set Balance="+sumToUpdate + " WHERE idAccount ="+m.getIdAcount()+" ");
+						
+						if (successQuery > 0)
+						{
+							//success in updating
+							try
+							{
+								// Get old balance
+								rs = state.executeQuery("select balance from account where idAccount =" +m.getIdAcount());
+								old1 = "0";
+								old2 = "0";
+								new1 = "0";
+								new2 = "0";
+								while(rs.next())
+								{
+									old1 = rs.getString(1);
+									new1 = old1;
+								}
+							}
+							catch(Exception e) {e.printStackTrace();}
+							
+							try
+							{
+								successQuery = state.executeUpdate("UPDATE Account set Balance="+sumToUpdate + " WHERE idAccount ="+m.getIdAcount()+" ");
+							}
+							catch(Exception e) {e.printStackTrace();}
+							
+							try
+							{
+								rs = state.executeQuery("select balance from account where idAccount =" +m.getIdAcount());
+								while(rs.next())
+								{
+									old2 = rs.getString(1);
+									new2 = old2;
+								}
+					
+								if (successQuery > 0)
+								{	
+									//success in updating
+									
+									// Insert this transaction into transaction table
+									String DATE_FORMAT_NOW = "yyyy-MM-dd";
+									String TIME_FORMAT_NOW = "HH:mm:ss";
+									Calendar cal = Calendar.getInstance();
+									SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_NOW);
+									String date = sdf.format(cal.getTime());
+									sdf = new SimpleDateFormat(TIME_FORMAT_NOW);
+									String time = sdf.format(cal.getTime());
+									System.out.println("Date: " +date +" time: " +time);
+					
+									String query = "insert into transaction (type, account1, account2, Date, Time, old_balance1, new_balance1, old_balance2, new_balance2) " +
+									"values ('W', '"+m.getIdAcount() +"', '" +m.getIdAcount() +"', '" +date +"', '" +time +"', '" +old1 +"', '" +new1 +"', '" +old2 +"', '" +new2 +"')";
+									
+									System.out.println(query);
+									state.executeUpdate(query);
+									
+									return new MessageResponse("Your Balance has been succesfully updated, the current balance is "+sumToUpdate);
+								}
+								else
+								{
+									//can't update
+									throw new  CannotUpdateException("Your Loan has not been updated, please try again");
+								}
+							}
+							catch (CannotUpdateException e)
+							{
+								//when it's not possible to update the balance
+								return new MessageResponse(e.getMessage());
+							}
+							catch(Exception e) 
+							{ 
+								e.printStackTrace();
+							}
+							
+							return new MessageResponse("Your Balance has been succesfully updated, the current balance is "+sumToUpdate);
+						
+						}
+						else
+						{
+							//can't update
+							throw new  CannotUpdateException("Your Balance has not been updated, please try again");
+						}
+					}
+				}
+				else
+				{
+					//the account number doesn't exist
+					throw new AccountNotFoundException();
+				}
+				}
+				catch (AccountNotFoundException e)
+				{
+					//when the Account is not found
+					return new MessageResponse(e.getMessage());
+				}
+				
+				catch (SumIncorrectException e)
+				{
+					//when a user entered a non valid sum of money
+					return new MessageResponse(e.getMessage());
+				}
+				catch(SQLException e)
+				{
+					System.err.println(e.getMessage());
+					System.err.println("  Error Message: " + e.getMessage());
+					//System.err.println(" Vendor Message: " + e.getErrorCode());
+				}
+				catch(Exception e) { e.printStackTrace(); }
+				
+				return null;
 		}
-		return null;
 	}
 
 	@Override
