5d0a292376f6859df3df14e20c4c77844bef332b
jeffreydmueller <jeffreydmueller@77c099dc-cc87-11dd-a8b6-f5aca7b582bb>
2009-02-20 00:11:34 +0000

    History tools etc

---
+++ BrickCityBankProject/src/brickcitybank/message/MessageHistory.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package brickcitybank.message;
+
+/**
+ * @author Caroline
+ *
+ */
+public class MessageHistory extends MessageOrder
+{
+
+	public MessageHistory(int id_user, int idAcount) 
+	{
+		super(id_user, idAcount);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;	
+	
+
+	
+}
