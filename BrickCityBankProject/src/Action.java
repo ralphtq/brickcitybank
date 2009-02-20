9f82e024cc724dc155694f7cefd3704b0e6f2ef9
caroline.malnoe <caroline.malnoe@77c099dc-cc87-11dd-a8b6-f5aca7b582bb>
2009-02-20 00:13:50 +0000

    Add history

--- BrickCityBankProject/src/Action.java
+++ BrickCityBankProject/src/Action.java
@@ -57,13 +57,47 @@ public class Action extends HttpServlet {
 		
 		PrintWriter out = response.getWriter();
 		
+		//echo the header and the menu
+		 String header ="<html><link rel=\"STYLESHEET\" type=\"text/css\" href=\"style.css\">"+
+	        "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=ISO-8859-1\">"+
+	        "<title>Brick City Bank</title>"+
+	        "</head><body><div id=\"header\">"+
+	        "<a id=\"ban-title\" href=\"welcome.jsp\">Brick City Bank</a>"+
+	        "</div>";
+	        out.write(header);
+	        
+	        //echo the menu
+	        String menu ="";
+	        
+	        
+	        menu += "<div id=\"menu\"><div class='element_menu'><h3>Log part</h3>";
+	        String user ="";
+	        if((session.getAttribute("userID")) == null){
+	        	menu += "<form action=\"http://localhost:8181/BrickCityBankProject/Login\" method=\"post\">";
+	        	menu += "	Username:<br/> <input type=\"text\" name=\"userName\"/><br/>";
+	        	menu += "	Password:<br/> <input type=\"password\" name=\"password\"/><br /><br />";
+	        	menu += "<input type=\"submit\" value=\"Login\"/></form>";
+	        }else{
+	        	user =(String)session.getAttribute("userID").toString();
+	        	menu +=("You are logged in "+ user);
+	        	// Log out
+	        	menu +=("<center><br><form action=\"Login\" method=\"POST\">");
+	        	menu +=("<input type=\"hidden\" name=\"logout\" value=\"logout\">");
+	        	menu +=("<input type=\"submit\" value=\"Logout\"></center></form>");
+	        }
+
+	        menu +=" </div></div>";
+	        out.write(menu);
+	        
 		
-		
+		//print content
+		out.write("<div id='content'>");
 		
 		//if it's a deposit
 		if(action.equals("deposit"))
 		{
 			//get the deposit amount
+			out.write("<h2>Result Deposit</h2>");
 			try{
 			depositAmount = Double.parseDouble(request.getParameter("depositAmount"));
 			}catch(Exception e){
@@ -72,11 +106,12 @@ public class Action extends HttpServlet {
 			}
 			out.write("Deposit amount: " +depositAmount);
 			MessageOrderMoney mom = new MessageOrderMoney(userID, accountID, ActionTool.DEPOSIT, depositAmount);
-			out.write("<br />" +serv.bankAction(mom).getResponse());
+			out.write("<br /><p>" +serv.bankAction(mom).getResponse()+"</p>");
 		}
 		else if(action.equals("withdraw"))
 		{
 			//if it's a withdrawal
+			out.write("<h2>Result Withdrawal</h2>");
 			try{
 			withdrawAmount = Double.parseDouble(request.getParameter("withdrawAmount"));
 			}catch(Exception e){
@@ -85,37 +120,55 @@ public class Action extends HttpServlet {
 			}
 			out.write("Withdraw amount: " +withdrawAmount);
 			MessageOrderMoney mom = new MessageOrderMoney(userID, accountID, ActionTool.WITHDRAW, withdrawAmount);
-			out.write("<br />" +serv.bankAction(mom).getResponse());		
+			out.write("<br /><p>" +serv.bankAction(mom).getResponse()+"</p>");		
 		}
 		else if(action.equals("transfer"))
 		{
 			//it's a tranfert
+			out.write("<h2>Result Transfer</h2>");
 			System.out.println("Action - Transfer");
 			try{
 				transferAmount = Double.parseDouble(request.getParameter("transferAmount"));
 				destAccount = Integer.parseInt(request.getParameter("destAccount"));
 			}catch(Exception e){
-				out.write("Transfert amount forced to 0");
+				out.write("<p>Transfert amount forced to 0</p>");
 				transferAmount = 0;
 				destAccount = 0;
 			}
-			out.write("Operation : Transfer amount: " +transferAmount+ "  to acount " + destAccount);
+			out.write("<p>Operation : Transfer amount: " +transferAmount+ "  to acount " + destAccount+"</p>");
 			
 			
 			//create message and send it to the server
 			MessageTransfer mt = new MessageTransfer(userID, accountID, transferAmount, destAccount);
 			System.out.println("Action - trying serv.bankAction()");
 			// THIS DOESNT DO ANYTHING!!!! returns null
-			out.write("<br />" +serv.bankAction(mt).getResponse());		
+			out.write("<br /><p>" +serv.bankAction(mt).getResponse()+"</p>");		
 			//
 			System.out.println("Action - done");
 		}
-		
+		else if(action.equals("history"))
+		{
+			//it's a tranfert
+			out.write("<h2>Result History</h2>");
+			
+			
+			
+			//create message and send it to the server
+			MessageHistory mh = new MessageHistory(userID, accountID);
+			
+			// THIS DOESNT DO ANYTHING!!!! returns null
+			out.write("<br /><p>" +serv.bankAction(mh).getResponse()+"</p>");		
+			//
+			System.out.println("Action - done");
+		}
 		// Return user to Accounts Summary page (Login servlet)
 		out.write("<form action=\"Login\" method=\"POST\">");
 		out.write("<input type=\"submit\" value=\"Continue\">");
 		out.write("</form>");
 		
+		
+		//close the page
+		out.write("</div></body></html>");
 	}
 
 }
